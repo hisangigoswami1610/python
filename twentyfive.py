f0a43720c49498e23715e3c909439dc66055675a
hisangigoswami1610 <gosaihisangi@gmail.com>
2026-07-28 10:50:40 +0530

    Add files via upload

---
+++ twentyfive.py
@@ -0,0 +1,10 @@
+student_details():
+ name = "Yash"
+ age = 21
+ course = "BCA"
+ return name, age, course
+# Receiving multiple values
+n, a, c = student_details()
+print("Name:", n)
+print("Age:", a)
+print("Course:", c)
